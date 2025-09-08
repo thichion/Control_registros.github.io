934d216179f7522bca1f1a28c58c71f2c508c902
thichion <carlosolarte4321@gmail.com>
2025-09-08 11:08:45 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-        import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";   
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";   
 
     const supabaseUrl = "https://otvcwvnlndxtzzmeqtcw.supabase.co";
     const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90dmN3dm5sbmR4dHp6bWVxdGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3NTg0OTQsImV4cCI6MjA2MzMzNDQ5NH0.psGUAZjKc_Ic9CFeumOIwS5DNWkgtABNZlcN0iig0cE";
@@ -212,3 +212,4 @@
             });
         }
     });
+
